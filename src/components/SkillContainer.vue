8dd4652d011bdd77dd7142fb9b8a971f4bbff4f7
Cameron-Ord <cameron.ian.ronald.ord@outlook.com>
2024-02-22 21:47:07 -0700

    remembering selections

--- src/components/SkillContainer.vue
+++ src/components/SkillContainer.vue
@@ -2,88 +2,99 @@
 import { onBeforeMount, ref, type Ref } from 'vue';
 import { skill_state } from '../stores/skill_state';
 const skill_inst = skill_state();
-const skill_list: Ref<Array<{skillName: string; skillValue: number}>> = ref(new Array())
+const skill_list: Ref<Array<{ skillName: string; skillValue: number }>> = ref(new Array())
 
 const plus = '/svgs/plus.svg';
 const minus = '/svgs/minus.svg';
 
 const increase_skill = (i: number, event: MouseEvent | null) => {
-    const lmtr: number = skill_inst.get_skill_limiter();
-    const s_len: number = skill_inst.get_skill_list_len();
-    if(i > s_len || i < 0){
-        return
-    }
-    if(!event){
-        return
-    }
-    if(lmtr >= 0 && lmtr < 1280){
-        skill_inst.increase_skill(i, event);
-        const new_list = skill_inst.get_skill_list();
-        skill_list.value = new Array();
-        skill_list.value = new_list;
-    }
+  const lmtr: number = skill_inst.get_skill_limiter();
+  const s_len: number = skill_inst.get_skill_list_len();
+  if (i > s_len || i < 0) {
+    return
+  }
+  if (!event) {
+    return
+  }
+  if (lmtr >= 0 && lmtr < 1280) {
+    skill_inst.increase_skill(i, event);
+    const new_list = skill_inst.get_skill_list();
+    skill_list.value = new Array();
+    skill_list.value = new_list;
+  }
 }
 
 const decrease_skill = (i: number, event: MouseEvent | null) => {
-    const lmtr: number = skill_inst.get_skill_limiter();
-    const s_len: number = skill_inst.get_skill_list_len();
-    if(i > s_len || i < 0){
-        return
-    }
-    if(!event){
-        return
-    }
-    if(lmtr > 0 && lmtr <= 1280){
-        skill_inst.decrease_skill(i, event);
-        const new_list = skill_inst.get_skill_list();
-        skill_list.value = new Array();
-        skill_list.value = new_list;
-    }
+  const lmtr: number = skill_inst.get_skill_limiter();
+  const s_len: number = skill_inst.get_skill_list_len();
+  if (i > s_len || i < 0) {
+    return
+  }
+  if (!event) {
+    return
+  }
+  if (lmtr > 0 && lmtr <= 1280) {
+    skill_inst.decrease_skill(i, event);
+    const new_list = skill_inst.get_skill_list();
+    skill_list.value = new Array();
+    skill_list.value = new_list;
+  }
 }
 
-onBeforeMount(()=>{
+const get_list = () => {
+  const remembered_list: any = skill_inst.load_from_cookies();
+  if (remembered_list == null) {
     skill_list.value = skill_inst.get_skill_list();
+  } else {
+    skill_list.value = remembered_list;
+  }
+}
+
+onBeforeMount(() => {
+  get_list();
 })
 
 </script>
 
 <template>
-    <div class="skills_div">
-        <div v-for="(skill, i) in skill_list" :key="i" class="loop_div"> 
-            <p class="skill_name">{{ skill.skillName }}</p>
-            <div class="icon_value_div">
-                <p>{{ skill.skillValue }}</p>
-                <img @click="increase_skill(i, $event)" :src="plus" alt="" class="svg">
-                <img @click="decrease_skill(i, $event)" :src="minus" alt="" class="svg">
-            </div>
-        </div>
+  <div class="skills_div">
+    <div v-for="(skill, i) in skill_list" :key="i" class="loop_div">
+      <p class="skill_name">{{ skill.skillName }}</p>
+      <div class="icon_value_div">
+        <p>{{ skill.skillValue }}</p>
+        <img @click="increase_skill(i, $event)" :src="plus" alt="" class="svg">
+        <img @click="decrease_skill(i, $event)" :src="minus" alt="" class="svg">
+      </div>
     </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
-.skills_div{
-    display: grid;
-    width: 80%;
-    row-gap: 25px;
-    overflow-y: auto;
-    max-height: 350px;
-    >.loop_div{
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        flex-wrap: wrap;
-        column-gap: 25px;
+.skills_div {
+  display: grid;
+  width: 80%;
+  row-gap: 25px;
+  overflow-y: auto;
+  max-height: 350px;
 
-        >.skill_name{
-            max-width: 125px;
-        }
-        >.icon_value_div{
-            display: flex;
-            align-items: center;
-            flex-direction: row;
-            column-gap: 15px;
-            justify-content: space-evenly;
-        }
+  >.loop_div {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    column-gap: 25px;
+
+    >.skill_name {
+      max-width: 125px;
     }
+
+    >.icon_value_div {
+      display: flex;
+      align-items: center;
+      flex-direction: row;
+      column-gap: 15px;
+      justify-content: space-evenly;
+    }
+  }
 }
 </style>
--- src/components/StatContainer.vue
+++ src/components/StatContainer.vue
@@ -2,89 +2,101 @@
 import { onBeforeMount, ref, type Ref } from 'vue';
 import { stat_state } from '../stores/stat_state'
 const stat_inst = stat_state();
-const stat_list: Ref<Array<{statName: string; statValue: number}>> = ref(new Array())
+const stat_list: Ref<Array<{ statName: string; statValue: number }>> = ref(new Array())
 
 const plus = '/svgs/plus.svg';
 const minus = '/svgs/minus.svg';
 
-const decrease_stat = (i: number, event: MouseEvent | null) =>{
-    const lmtr: number = stat_inst.get_limiter_value();
-    const s_len: number = stat_inst.get_stat_list_len();
-    if(i > s_len || i < 0){
-        return
-    }
-    if(!event){
-        return
-    }
-    if(lmtr > 21 && lmtr <= 46){
-        stat_inst.decrease_stat(i, event);
-        const new_list = stat_inst.get_stat_list();
-        stat_list.value = new Array();
-        stat_list.value = new_list;
-    }
+const decrease_stat = (i: number, event: MouseEvent | null) => {
+  const lmtr: number = stat_inst.get_limiter_value();
+  const s_len: number = stat_inst.get_stat_list_len();
+  if (i > s_len || i < 0) {
+    return
+  }
+  if (!event) {
+    return
+  }
+  if (lmtr > 21 && lmtr <= 46) {
+    stat_inst.decrease_stat(i, event);
+    const new_list = stat_inst.get_stat_list();
+    stat_list.value = new Array();
+    stat_list.value = new_list;
+  }
 }
 
 const increase_stat = (i: number, event: MouseEvent | null) => {
-    const lmtr: number = stat_inst.get_limiter_value();
-    const s_len: number = stat_inst.get_stat_list_len();
-    if(i > s_len || i < 0){
-        return
-    }
-    if(!event){
-        return
-    }
-    if(lmtr >= 21 && lmtr < 46){
-        stat_inst.increase_stat(i, event);
-        const new_list = stat_inst.get_stat_list();
-        stat_list.value = new Array();
-        stat_list.value = new_list;
-    }
+  const lmtr: number = stat_inst.get_limiter_value();
+  const s_len: number = stat_inst.get_stat_list_len();
+  if (i > s_len || i < 0) {
+    return
+  }
+  if (!event) {
+    return
+  }
+  if (lmtr >= 21 && lmtr < 46) {
+    stat_inst.increase_stat(i, event);
+    const new_list = stat_inst.get_stat_list();
+    stat_list.value = new Array();
+    stat_list.value = new_list;
+  }
 }
 
-onBeforeMount(()=>{
+const get_list = () => {
+  const remembered_list: any = stat_inst.load_from_cookies();
+  if (remembered_list == null) {
     stat_list.value = stat_inst.get_stat_list();
+  } else {
+    stat_list.value = remembered_list;
+  }
+}
+
+onBeforeMount(() => {
+  get_list();
 })
 
 </script>
 
 <template>
-    <div class="stats_div">
-        <div v-for="(stat, i) in stat_list" :key="i" class="loop_div">
-            <p class="stat_name">{{ stat.statName }}</p>
-            <div class="icon_value_div">
-                <p>{{ stat.statValue }}</p>
-                <img @click="increase_stat(i, $event)" :src="plus" alt="" class="svg">
-                <img @click="decrease_stat(i, $event)" :src="minus" alt="" class="svg">
-            </div>
-        </div>
+  <div class="stats_div">
+    <div v-for="(stat, i) in stat_list" :key="i" class="loop_div">
+      <p class="stat_name">{{ stat.statName }}</p>
+      <div class="icon_value_div">
+        <p>{{ stat.statValue }}</p>
+        <img @click="increase_stat(i, $event)" :src="plus" alt="" class="svg">
+        <img @click="decrease_stat(i, $event)" :src="minus" alt="" class="svg">
+      </div>
     </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
-.stats_div{
-    display: grid;
-    width: 80%;
-    row-gap: 25px;
-    overflow-y: auto;
-    max-height: 350px;
-    >.loop_div{
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        flex-direction: row;
-        flex-wrap: wrap;
-        column-gap: 25px;
-        row-gap: 5px;
-        >.stat_name{
-            max-width: 125px;
-        }
-        >.icon_value_div{
-            align-items: center;
-            display: flex;
-            flex-direction: row;
-            column-gap: 15px;
-            justify-content: space-evenly;
-        }
+.stats_div {
+  display: grid;
+  width: 80%;
+  row-gap: 25px;
+  overflow-y: auto;
+  max-height: 350px;
+
+  >.loop_div {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    flex-direction: row;
+    flex-wrap: wrap;
+    column-gap: 25px;
+    row-gap: 5px;
+
+    >.stat_name {
+      max-width: 125px;
     }
+
+    >.icon_value_div {
+      align-items: center;
+      display: flex;
+      flex-direction: row;
+      column-gap: 15px;
+      justify-content: space-evenly;
+    }
+  }
 }
 </style>
